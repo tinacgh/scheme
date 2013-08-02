70dcc2d4b9196771d57fe3ae8f97406342640add
Tina home ubuntu <tinacgh@gmail.com>
2013-08-01 21:59:35 -0300

    added circle

--- concabs/ch01.scm
+++ concabs/ch01.scm
@@ -12,5 +12,17 @@
   (stack (side-by-side (turn-right img) (half-turn img))
 	 (side-by-side img (turn-left img))))
 
-(define (draw-segment x1 y1 . pts)
-  (line x1 y1 (car pts) (cadr pts)))
+;;; (draw-segment 0.0 0.0 0.1 0.1 0.2 0.2)
+;;; (draw-line
+
+(define (draw-segment . pts)
+  (if (< (length pts) 5)
+      (line (car pts) (cadr pts) (caddr pts) (cadddr pts))
+      (overlay (line (car pts) (cadr pts) (caddr pts) (cadddr pts)) (apply draw-segment (cddr pts)))))
+
+(define circle
+  (let ((param-t (map (lambda (x) (* 2 3.1415926 (/ x 35))) (range 36))))
+    (apply draw-segment (interleave (map sin param-t) (map cos param-t)))))
+
+;;; NEXT: filled circle
+;;; for each segment, fill a triangle like a slice of pizza
---
+++ concabs/util.scm
@@ -0,0 +1,12 @@
+(define (interleave a b)
+  (if (null? a) b
+      (if (null? b) a
+	  (cons (car a) (cons (car b) (interleave (cdr a) (cdr b)))))))
+
+(define (rev-range n)
+  (if (= n 0)
+      '()
+      (cons (- n 1) (rev-range (- n 1)))))
+
+(define (range n)
+  (reverse (rev-range n)))
