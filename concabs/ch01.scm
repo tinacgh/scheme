d0cf6c2c5b62c311f273b9cf4d4cf9df6c3cf959
Tina home ubuntu <tinacgh@gmail.com>
2013-08-02 00:32:03 -0300

    scaled filled circle

--- concabs/ch01.scm
+++ concabs/ch01.scm
@@ -20,9 +20,25 @@
       (line (car pts) (cadr pts) (caddr pts) (cadddr pts))
       (overlay (line (car pts) (cadr pts) (caddr pts) (cadddr pts)) (apply draw-segment (cddr pts)))))
 
+(define (fill-to-center . pts)
+  (if (< (length pts) 5)
+      (filled-triangle (car pts) (cadr pts) (caddr pts) (cadddr pts) 0 0)
+      (overlay (filled-triangle (car pts) (cadr pts) (caddr pts) (cadddr pts) 0 0) (apply fill-to-center (cddr pts)))))
+
 (define circle
   (let ((param-t (map (lambda (x) (* 2 3.1415926 (/ x 35))) (range 36))))
     (apply draw-segment (interleave (map sin param-t) (map cos param-t)))))
 
+(define (scaled-circle horiz vert)
+  (let ((param-t (map (lambda (x) (* 2 3.1415926 (/ x 35))) (range 36))))
+    (apply draw-segment (interleave (map (lambda (x) (* horiz (sin x))) param-t) (map (lambda (x) (* vert (cos x))) param-t)))))
+
 ;;; NEXT: filled circle
 ;;; for each segment, fill a triangle like a slice of pizza
+(define filled-circle
+  (let ((param-t (map (lambda (x) (* 2 3.1415926 (/ x 35))) (range 36))))
+    (apply fill-to-center (interleave (map sin param-t) (map cos param-t)))))
+
+(define (scaled-filled-circle horiz vert)
+  (let ((param-t (map (lambda (x) (* 2 3.1415926 (/ x 35))) (range 36))))
+    (apply fill-to-center (interleave (map (lambda (x) (* horiz (sin x))) param-t) (map (lambda (x) (* vert (cos x))) param-t)))))
